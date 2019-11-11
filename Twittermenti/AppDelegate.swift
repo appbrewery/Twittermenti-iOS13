c115f445434cd31234daac71d4d9002da503113c
TheMuellenator <TheMuellenator@users.noreply.github.com>
2019-11-11 15:34:29 +0000

    Updating iOS13

--- Twittermenti/AppDelegate.swift
+++ Twittermenti/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Twittermenti
 //
-//  Created by Angela Yu on 17/07/2018.
-//  Copyright © 2018 London App Brewery. All rights reserved.
+//  Created by Angela Yu on 17/07/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
